5bbac11890ddfee3ed536d5ed39e94de0ad83abb
Bob <alexnavin@outlook.com>
2023-02-05 14:14:34 +0600

    base CRUD optioons whith todos

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,9 +1,28 @@
 from rest_framework import serializers
-
 from .models import Todo
 
 
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todo
-        fields = ('category', 'title')
+class TodoSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    category_id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+    description = serializers.CharField()
+    date_created = serializers.DateTimeField(read_only=True)
+    date_expiration = serializers.DateTimeField()
+    date_completed = serializers.DateTimeField(read_only=True)
+    is_important = serializers.BooleanField(default=False)
+
+    def create(self, validated_data):
+        return Todo.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.user_id = validated_data.get('user_id', instance.user_id)
+        instance.category_id = validated_data.get('category_id', instance.category_id)
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.date_created = validated_data.get('date_created', instance.date_created)
+        instance.date_expiration = validated_data.get('date_expiration', instance.date_expiration)
+        instance.date_completed = validated_data.get('date_completed', instance.date_completed)
+        instance.is_important = validated_data.get('is_important', instance.is_important)
+        instance.save()
+        return instance
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Todo
+from .models import Todo, Category
 from .serializers import TodoSerializer
 
 
-class TodoAPIView(generics.ListAPIView):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+class TodoAPIView(APIView):
+    def get(self, request):
+        data = Todo.objects.all()
+        return Response({'todos': TodoSerializer(data, many=True).data})
 
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'todo_new': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+        try:
+            instance = Todo.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Objects does not exists'})
+
+        serializer = TodoSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'todo': serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method DELETE not allowed'})
+        try:
+            instance = Todo.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exists'})
+
+        instance.delete()
+
+        return Response({'todo': 'Delete todo ' + str(pk)})
