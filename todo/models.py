0a1d4969e3c9c95a2b073797e5fd9f174a8245f7
Bob <alexnavin@outlook.com>
2023-03-28 12:37:25 +0600

    models edited

--- todo/models.py
+++ todo/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
 
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-        ordering = ['-id']
+        ordering = ['name']
 
     def __str__(self):
         return self.name
